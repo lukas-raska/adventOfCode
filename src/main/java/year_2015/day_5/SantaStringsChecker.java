eda1541544408bfee20a3d6f74dd0c7da659d47a
lukas-raska <luky.raska@gmail.com>
2024-06-15 21:36:07 +0200

    2015/5: add containsAtLeastThreeVowels method in SantaStingChecker

--- src/main/java/year_2015/day_5/SantaStringsChecker.java
+++ src/main/java/year_2015/day_5/SantaStringsChecker.java
@@ -2,5 +2,15 @@ package year_2015.day_5;
 
 public class SantaStringsChecker {
 
+    private static final String VOWELS = "aeiou";
 
+     boolean containsAtLeastThreeVowels(String string) {
+
+        long vowelsCount = string
+                .chars()
+                .filter(c -> VOWELS.indexOf(c) >= 0)
+                .count();
+
+        return vowelsCount >= 3;
+    }
 }
