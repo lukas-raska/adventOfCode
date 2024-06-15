3f1778a1069a70eb615f445a145dd62a45d5eb26
lukas-raska <luky.raska@gmail.com>
2024-06-15 21:23:58 +0200

    2015/5 initial commit

---
+++ src/main/java/year_2015/day_5/Main.java
@@ -0,0 +1,8 @@
+package year_2015.day_5;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/year_2015/day_5/SantaStringsChecker.java
@@ -0,0 +1,6 @@
+package year_2015.day_5;
+
+public class SantaStringsChecker {
+
+
+}
