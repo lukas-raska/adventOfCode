bf9ac794385c219aaf231ca10b6d65b15529629a
lukas-raska <luky.raska@gmail.com>
2024-06-16 05:30:24 +0200

    2015/5: add isNice method

--- src/main/java/year_2015/day_5/SantaStringsChecker.java
+++ src/main/java/year_2015/day_5/SantaStringsChecker.java
@@ -3,9 +3,9 @@ package year_2015.day_5;
 public class SantaStringsChecker {
 
     private static final String VOWELS = "aeiou";
-    private static final String [] EXCLUDED_STRINGS = new String[] {"ab","cd","pq","xy"}    ;
+    private static final String[] EXCLUDED_STRINGS = new String[]{"ab", "cd", "pq", "xy"};
 
-     boolean containsAtLeastThreeVowels(String string) {
+    boolean containsAtLeastThreeVowels(String string) {
 
         long vowelsCount = string
                 .chars()
@@ -17,25 +17,32 @@ public class SantaStringsChecker {
 
     boolean containsDoubledLetter(String string) {
 
-         for (int i = 0; i < string.length()-1; i++) {
-            String tested = string.substring(i, i+2);
-            if (tested.charAt(0)==tested.charAt(1)) {
+        for (int i = 0; i < string.length() - 1; i++) {
+            String tested = string.substring(i, i + 2);
+            if (tested.charAt(0) == tested.charAt(1)) {
                 return true;
             }
         }
         return false;
     }
 
-    boolean containsGivenStrings(String string, String [] comparisons){
-         for (String comparison : comparisons) {
-             if (string.contains(comparison)) {
-                 return true;
-             }
-         }
-         return false;
+    boolean containsGivenStrings(String string,
+                                 String[] comparisons) {
+        for (String comparison : comparisons) {
+            if (string.contains(comparison)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    boolean doesNotContainsExcludedStrings(String string){
-         return !containsGivenStrings(string, EXCLUDED_STRINGS);
+    boolean doesNotContainsExcludedStrings(String string) {
+        return !containsGivenStrings(string, EXCLUDED_STRINGS);
+    }
+
+    boolean isNice(String string) {
+        return containsAtLeastThreeVowels(string) &&
+                containsDoubledLetter(string) &&
+                doesNotContainsExcludedStrings(string);
     }
 }
