55e61ee4a4bb8792e978eadea726962ee1c4d37d
lukas-raska <luky.raska@gmail.com>
2024-06-16 05:35:04 +0200

    2015/5: add test data in main

--- src/main/java/year_2015/day_5/Main.java
+++ src/main/java/year_2015/day_5/Main.java
@@ -2,7 +2,17 @@ package year_2015.day_5;
 
 public class Main {
 
+    private static final String[] TEST_DATA = {"ugknbfddgicrmopn", "jchzalrnumimnmhp", "haegwjzuvuyypxyu",
+            "dvszwmarrgswjxmb"};
+
     public static void main(String[] args) {
+
+        SantaStringsChecker checker = new SantaStringsChecker();
+
+        for (String data : TEST_DATA) {
+            System.out.println(data + " is nice? : " + checker.isNice(data));
+        }
+
 
     }
 }
