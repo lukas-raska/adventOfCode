659f837f8dc137c1a830c637a0c8c399e73cef47
lukas-raska <luky.raska@gmail.com>
2024-06-16 05:43:54 +0200

    2015/5: part one solved

--- src/main/java/year_2015/day_5/Main.java
+++ src/main/java/year_2015/day_5/Main.java
@@ -1,18 +1,37 @@
 package year_2015.day_5;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.util.Scanner;
+
 public class Main {
 
     private static final String[] TEST_DATA = {"ugknbfddgicrmopn", "jchzalrnumimnmhp", "haegwjzuvuyypxyu",
             "dvszwmarrgswjxmb"};
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         SantaStringsChecker checker = new SantaStringsChecker();
 
-        for (String data : TEST_DATA) {
-            System.out.println(data + " is nice? : " + checker.isNice(data));
+//        for (String data : TEST_DATA) {
+//            System.out.println(data + " is nice? : " + checker.isNice(data));
+//        }
+
+        File input = new File("src/main/java/year_2015/day_5/input.txt");
+        Scanner scanner = new Scanner(input);
+
+        int counter = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (checker.isNice(line)){
+                counter++;
+            }
         }
 
+        System.out.println("Answer of day 5: ");
+        System.out.println("Part one: " + counter);
 
     }
 }
