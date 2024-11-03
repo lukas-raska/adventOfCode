ef2d8dda1ca72b552be27621f2a40f19628f48e2
lukas-raska <luky.raska@gmail.com>
2024-11-03 16:08:49 +0100

    2015/8 part 2 solved

--- src/main/java/year_2015/day_8/Main.java
+++ src/main/java/year_2015/day_8/Main.java
@@ -18,11 +18,21 @@ public class Main {
         System.out.println("Answer of the day 8: ");
         System.out.println("Part 1: " + part1result);
 
+        int originalLengthsSum = inputData.stream()
+                .mapToInt(String::length)
+                .sum();
 
+        int lengthsWithAddedQuotes = inputData.stream()
+                .map(Main::addEscapes)
+                .mapToInt(String::length)
+                .sum();
+
+        int part2result = lengthsWithAddedQuotes - originalLengthsSum;
+
+        System.out.println("Part 2: " + part2result); //2117
     }
 
     static String removeQuotesAndEscapes(String string) {
-
         String modified = string.substring(1, string.length() - 1); //removes '"' at the first and last place
 
         modified = modified.replaceAll("\\\\x[0-9A-Fa-f]{2}", "*"); //replacing '\x--'
@@ -30,6 +40,24 @@ public class Main {
         modified = modified.replaceAll("\\\\\\\\", "*"); //replacing "\\"
 
         return modified;
+    }
+
+
+    static String addEscapes(String string) {
+
+        StringBuilder modified = new StringBuilder("\"");
+
+        for (char c : string.toCharArray()) {
+            modified.append(
+                    switch (c) {
+                        case '\"' -> "\\\"";
+                        case '\\' -> "\\\\";
+                        default -> "" + c;
+                    });
+        }
+        modified.append("\"");
+
+        return modified.toString();
     }
 
 
