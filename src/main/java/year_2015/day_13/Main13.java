4edff12c7fbf90d96e6401a46314aca56adf32c3
lukas-raska <luky.raska@gmail.com>
2024-11-13 21:13:30 +0100

    2015/13 part2 solution

--- src/main/java/year_2015/day_13/Main13.java
+++ src/main/java/year_2015/day_13/Main13.java
@@ -8,12 +8,30 @@ public class Main13 {
 
         var puzzleInput = InputLoader.load(2015, 13, "input_2015_13.txt");
 
-        TableManager tableManager = new TableManager (new Table(puzzleInput));
+        TableManager tableManager = new TableManager(new Table(puzzleInput));
 
         int resultPart1 = tableManager.countOptimalHappiness();
 
+        //part 2
+        Person myself = new Person("Me");
+        tableManager
+                .getTable()
+                .getPersonList()
+                .stream()
+                .forEach(person -> {
+                    myself.getSympathyMap().put(person.getName(), 0);
+                    person.getSympathyMap().put("Me", 0);
+                });
+        tableManager
+                .getTable()
+                .getPersonList()
+                .add(myself);
+
+        int resultPart2 = tableManager.countOptimalHappiness();
+
         System.out.println("The answer of the day 13: ");
         System.out.println("Part 1: " + resultPart1);
+        System.out.println("Part 2: " + resultPart2);
 
     }
 
