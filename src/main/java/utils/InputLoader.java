3d50dbb80c4a1290808b6d8171579972e6b31e1a
lukas-raska <luky.raska@gmail.com>
2024-12-03 04:46:36 +0100

    change of InputLoader - loading from resources

--- src/main/java/utils/InputLoader.java
+++ src/main/java/utils/InputLoader.java
@@ -8,10 +8,9 @@ import java.util.List;
 public class InputLoader {
 
     public static List<String> load(int year,
-                                    int day,
-                                    String fileName) {
+                                    int day) {
 
-        Path filePath = Path.of("src/main/java/year_" + year + "/day_" + day + "/" + fileName);
+        Path filePath = Path.of("src/main/resources/" + year + "/input_" + year + "_" + day + ".txt");
 
         try {
             return Files.readAllLines(filePath);
