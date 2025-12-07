543dd5333fe8179caf83b69d0da87abf18718061
lukas-raska <luky.raska@gmail.com>
2025-12-07 15:06:26 +0100

    add Solver interface

---
+++ src/main/java/Solver.java
@@ -0,0 +1,19 @@
+public interface Solver<T1, T2> {
+
+    <T> T part1();
+
+    <T> T part2();
+
+    default <T> void printSolution() {
+        long start = System.nanoTime();
+        T result1 = part1();
+        long elapsedMillis = Math.round((System.nanoTime() - start) / 1_000_000.);
+        System.out.println("Part 1: " + result1 + " (elapsed: " + elapsedMillis + ")");
+        start = System.nanoTime();
+        T result2 = part2();
+        elapsedMillis = Math.round((System.nanoTime() - start) / 1_000_000.);
+        System.out.println("Part 2: " + result2 + " (elapsed: " + elapsedMillis + ")");
+    }
+
+
+}
