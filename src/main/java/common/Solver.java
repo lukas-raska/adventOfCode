1f946f81672025efe75d86c7b0c8394298f42f08
lukas-raska <luky.raska@gmail.com>
2025-12-11 20:19:27 +0100

    add common Solver interface

--- src/main/java/common/Solver.java
+++ src/main/java/common/Solver.java
@@ -10,11 +10,11 @@ public interface Solver<T1,T2> {
         long start = System.nanoTime();
         T1 result1 = part1();
         long elapsedMillis = Math.round((System.nanoTime() - start) / 1_000_000.);
-        System.out.println("Part 1: " + result1 + " (elapsed: " + elapsedMillis + ")");
+        System.out.println("Part 1: " + result1 + " (elapsed: " + elapsedMillis + " ms)");
         start = System.nanoTime();
         T2 result2 = part2();
         elapsedMillis = Math.round((System.nanoTime() - start) / 1_000_000.);
-        System.out.println("Part 2: " + result2 + " (elapsed: " + elapsedMillis + ")");
+        System.out.println("Part 2: " + result2 + " (elapsed: " + elapsedMillis + " ms)");
     }
 
 
