648d03ff96b5017469ff232ac552cca3c20d10f6
lukas-raska <luky.raska@gmail.com>
2026-01-01 17:00:31 +0100

    2025, day 10, part 1 solution

--- src/main/java/year_2025/day_10/Day10Solver.java
+++ src/main/java/year_2025/day_10/Day10Solver.java
@@ -2,25 +2,64 @@ package year_2025.day_10;
 
 import common.Solver;
 
-import java.util.List;
+import java.util.*;
 
 public class Day10Solver implements Solver<Integer, Integer> {
 
     private final List<Machine> machines;
 
-    public Day10Solver (List<String> inputData){
+
+    public Day10Solver(List<String> inputData) {
         this.machines = inputData.stream()
                 .map(Machine::parse)
                 .toList();
     }
 
-    private int getMinPresses(){
-        return 0;
+
+    private String getToggledLights(int[] button,
+                                    String lights) {
+        StringBuilder toggled = new StringBuilder(lights);
+        for (int index : button) {
+            toggled.setCharAt(index, lights.charAt(index) == '#' ? '.' : '#');
+        }
+        return toggled.toString();
     }
 
     @Override
     public Integer part1() {
-        return 0;
+        int sum = 0;
+        for (Machine machine : machines) {
+            int cnt = 0;
+            Deque<String> current = new LinkedList<>();
+            Deque<String> next = new LinkedList<>();
+            Set<String> visited = new HashSet<>();
+            String start = ".".repeat(machine.lights().length());
+            visited.add(start);
+            current.add(start);
+            boolean wasFound = false;
+            while (!wasFound) {
+                String lights = current.pop();
+                for (var btn : machine.buttons()) {
+                    String toggled = getToggledLights(btn, lights);
+                    if (toggled.equals(machine.lights())) {
+                        sum += cnt + 1;
+                        wasFound = true;
+                        break;
+                    }
+                    if (!visited.contains(toggled)) {
+                        next.add(toggled);
+                        visited.add(toggled);
+                    }
+                }
+                if (current.isEmpty()) {
+                    cnt++;
+                    current = next;
+                    next = new LinkedList<>();
+                }
+            }
+        }
+        return sum;
+
     }
 
     @Override
--- src/main/java/year_2025/day_10/Machine.java
+++ src/main/java/year_2025/day_10/Machine.java
@@ -5,27 +5,28 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-public record Machine(char[]lights, List<int[]>buttons, int[] joltage) {
+public record Machine(String lights, List<int[]> buttons, int[] joltage) {
 
-    public static Machine parse(String data){
-        char [] lights =null;
+    public static Machine parse(String data) {
+        String lights = null;
         List<int[]> buttons = null;
-        int [] joltage = null;
-        for(String s: data.split(" ")){
-            if (s.startsWith("[")){
-                lights = s.replaceAll("[\\[\\]]","").toCharArray();
+        int[] joltage = null;
+        for (String s : data.split(" ")) {
+            if (s.startsWith("[")) {
+                lights = s.replaceAll("[\\[\\]]", "");
+
             }
-            if (s.startsWith("(")){
-                int[] button = Arrays.stream(s.replaceAll("[()]","").split(","))
+            if (s.startsWith("(")) {
+                int[] button = Arrays.stream(s.replaceAll("[()]", "").split(","))
                         .mapToInt(Integer::parseInt)
                         .toArray();
-                if (buttons == null){
+                if (buttons == null) {
                     buttons = new ArrayList<>();
                 }
                 buttons.add(button);
             }
-            if (s.startsWith("{")){
-                 joltage = Arrays.stream(s.replaceAll("[{}]", "").split(","))
+            if (s.startsWith("{")) {
+                joltage = Arrays.stream(s.replaceAll("[{}]", "").split(","))
                         .mapToInt(Integer::parseInt)
                         .toArray();
             }
@@ -34,6 +35,6 @@ public record Machine(char[]lights, List<int[]>buttons, int[] joltage) {
         Objects.requireNonNull(joltage);
         Objects.requireNonNull(buttons);
 
-        return new Machine(lights,buttons,joltage);
+        return new Machine(lights, buttons, joltage);
     }
 }
