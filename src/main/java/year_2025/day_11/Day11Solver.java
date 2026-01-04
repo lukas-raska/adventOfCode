352e0009d3a4b85fa3968c5c37793f6bee2eb7f8
lukas-raska <luky.raska@gmail.com>
2026-01-04 08:45:41 +0100

    2025, day11 initial

---
+++ src/main/java/year_2025/day_11/Day11Solver.java
@@ -0,0 +1,15 @@
+package year_2025.day_11;
+
+import common.Solver;
+
+public class Day11Solver implements Solver<Integer,Integer> {
+    @Override
+    public Integer part1() {
+        return 0;
+    }
+
+    @Override
+    public Integer part2() {
+        return 0;
+    }
+}
---
+++ src/main/java/year_2025/day_11/Main11.java
@@ -0,0 +1,19 @@
+package year_2025.day_11;
+
+public class Main11 {
+
+    public static void main(String[] args) {
+
+        String testData = """
+                aaa: you hhh
+                you: bbb ccc
+                bbb: ddd eee
+                ccc: ddd eee fff
+                ddd: ggg
+                eee: out
+                fff: out
+                ggg: out
+                hhh: ccc fff iii
+                iii: out""";
+    }
+}
