3b19717691c65b168bc33df3472f5ff37d15f364
lukas-raska <luky.raska@gmail.com>
2026-01-04 16:57:12 +0100

    2025, day 12 initial

---
+++ src/main/java/year_2025/day_12/Day12Solver.java
@@ -0,0 +1,15 @@
+package year_2025.day_12;
+
+import common.Solver;
+
+public class Day12Solver  implements Solver<Long,Long> {
+    @Override
+    public Long part1() {
+        return 0L;
+    }
+
+    @Override
+    public Long part2() {
+        return 0L;
+    }
+}
---
+++ src/main/java/year_2025/day_12/Main12.java
@@ -0,0 +1,9 @@
+package year_2025.day_12;
+
+public class Main12 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
